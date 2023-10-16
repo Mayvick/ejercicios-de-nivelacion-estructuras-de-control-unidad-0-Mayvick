c5c4dceb7322872e73a81a29d264f6fd9f292c92
Mayvick <147261961+Mayvick@users.noreply.github.com>
2023-10-15 20:01:00 -0600

    Update Ejercicio_3.cs

--- Ejercicio_3.cs
+++ Ejercicio_3.cs
@@ -1,5 +1,42 @@
 /*Ejercicio 3: Numero primo
 
+csharp
+using System;
 
-Escribe un
-programa que determine si un número dado es primo o no.*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        bool esPrimo = true;
+
+        if (numero <= 1)
+        {
+            esPrimo = false;
+        }
+        else
+        {
+            for (int i = 2; i <= Math.Sqrt(numero); i++)
+            {
+                if (numero % i == 0)
+                {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+
+        if (esPrimo)
+        {
+            Console.WriteLine(numero + " es un número primo.");
+        }
+        else
+        {
+            Console.WriteLine(numero + " no es un número primo.");
+        }
+
+        Console.ReadLine();
+    }
+}
