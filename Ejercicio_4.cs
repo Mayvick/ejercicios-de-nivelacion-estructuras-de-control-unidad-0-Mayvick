62c4b6e7b5fd9a8b3b92bb807d532c15b84b0e2d
Mayvick <147261961+Mayvick@users.noreply.github.com>
2023-10-15 20:03:33 -0600

    Update Ejercicio_4.cs

--- Ejercicio_4.cs
+++ Ejercicio_4.cs
@@ -1,5 +1,38 @@
 /*Ejercicio 4: Serie factorial
 
+csharp
+using System;
 
-Crea un programa que genere y muestre el
-resultado factorial de un numero. */
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        long factorial = CalcularFactorial(numero);
+
+        Console.WriteLine("El factorial de " + numero + " es: " + factorial);
+
+        Console.ReadLine();
+    }
+
+    static long CalcularFactorial(int numero)
+    {
+        if (numero == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            long factorial = 1;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
