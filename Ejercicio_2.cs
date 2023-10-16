1838d86ed9f7bf0162ae0c81bd3ca8ba2318aff1
Mayvick <147261961+Mayvick@users.noreply.github.com>
2023-10-15 19:58:51 -0600

    Update Ejercicio_2.cs

--- Ejercicio_2.cs
+++ Ejercicio_2.cs
@@ -1,5 +1,26 @@
 /*Ejercicio 2: Suma de Dígitos
 
+csharp
+using System;
 
-Crea un programa que calcule y muestre la
-suma de los dígitos de un número ingresado por el usuario.*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int suma = 0;
+
+        while (numero != 0)
+        {
+            int digito = numero % 10;
+            suma += digito;
+            numero /= 10;
+        }
+
+        Console.WriteLine("La suma de los dígitos es: " + suma);
+        
+        Console.ReadLine();
+    }
+}
